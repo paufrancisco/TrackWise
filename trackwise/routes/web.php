ae37523e209fffbe36162772aadf9847c718d02c
paufrancisco <francisco.paulo.pf@gmail.com>
2026-03-08 15:32:29 +0800

    clean web.php

--- trackwise/routes/web.php
+++ trackwise/routes/web.php
@@ -12,21 +12,17 @@ Route::get('/', function () {
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    // Must be defined before resource() to avoid route collision
-    Route::get('/transactions/export/csv',
-        [TransactionController::class, 'export'])->name('transactions.export');
+    Route::get('/transactions/export/pdf', [TransactionController::class, 'exportPdf'])
+        ->name('transactions.export.pdf');
+
+    Route::get('/transactions/export/excel', [TransactionController::class, 'exportExcel'])
+        ->name('transactions.export.excel');
 
     Route::resource('transactions', TransactionController::class);
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/transactions/export/pdf', [TransactionController::class, 'export'])
-    ->name('transactions.export.pdf');
-
-    Route::get('/transactions/export/excel', [TransactionController::class, 'exportExcel'])
-    ->name('transactions.export.excel');
 });
 
 require __DIR__.'/auth.php';
